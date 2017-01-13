13aba74712231c269913d88a87e037d1dfdc42fa
Frédéric Haller <fhaller75@gmail.com>
2017-01-14 00:38:24 +0100

    27. Filtering Out Albums

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -4,21 +4,17 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
-const IMAGES = [
-  { title: 'Pen', link: 'http://dummyimage.com/600x400' },
-  { title: 'Pine Tree', link: 'http://dummyimage.com/600x400' },
-  { title: 'Mug', link: 'http://dummyimage.com/600x400' },
-];
-
 // Create our component
-const ImageList = () => {
-  const RenderedImages = IMAGES.map(function(image) {
-    return <ImageDetail />
-  });
+const ImageList = (props) => {
+  const validImages = props.images.filter(image => !image.is_album);
+
+  const renderedImages = validImages.map(image => 
+    <ImageDetail key={image.title} image={image} />
+  );
 
   return (
-    <ul>
-      {RenderedImages}
+    <ul className="media-list list-group">
+      {renderedImages}
     </ul>
   );
 };
