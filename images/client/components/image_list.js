3424ff5e655a63ddcfb924fa57fd9a7fdfb847c7
Frédéric Haller <fhaller75@gmail.com>
2017-01-13 23:03:39 +0100

    15. Building a List of Images

---
+++ images/client/components/image_list.js
@@ -0,0 +1,27 @@
+// Create out image list component
+
+// Import React
+import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'http://dummyimage.com/600x400' },
+  { title: 'Pine Tree', link: 'http://dummyimage.com/600x400' },
+  { title: 'Mug', link: 'http://dummyimage.com/600x400' },
+];
+
+// Create our component
+const ImageList = () => {
+  const RenderedImages = IMAGES.map(function(image) {
+    return <ImageDetail />
+  });
+
+  return (
+    <ul>
+      {RenderedImages}
+    </ul>
+  );
+};
+
+// Export our component
+export default ImageList;
