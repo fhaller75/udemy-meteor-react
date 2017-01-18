39184dd8f8af3b4031fff3c454889c4c6c72aed9
Frédéric Haller <fhaller75@gmail.com>
2017-01-18 22:34:26 +0100

    74. Updating Mongo Records - End of section 4

--- shorten_my_link/server/main.js
+++ shorten_my_link/server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 import { Links } from '../imports/collections/links';
 
@@ -7,3 +9,25 @@ Meteor.startup(() => {
     return Links.find({});
   });
 });
+
+function onRoute(req, res, next) {
+  // Take the token out of the url
+  // Try to find a matiching link
+  const link = Links.findOne({ token: req.params.token });
+
+  if (link) {
+    // If found, redirect and increment the counter
+    Links.update( link, { $inc: { clicks: 1 }});
+    res.writeHead(307, { 'Location': link.url });
+    res.end();
+  } else {
+    // Else, send to our React app
+    next();
+  }
+}
+
+const middleware = ConnectRoute(function(router) {
+  router.get('/:token', onRoute);
+})
+
+WebApp.connectHandlers.use(middleware);
