7435b2269ae8294e2c9fa1797686f06ac16aa622
Frédéric Haller <fhaller75@gmail.com>
2017-01-21 01:18:38 +0100

    87. Claiming Bins with Subscriptions

---
+++ markbin/client/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/app';
+import { Bins } from '../imports/collections/bins';
+
+Meteor.startup(() => {
+  ReactDom.render(<App/>, document.querySelector('.render-target'));
+});
