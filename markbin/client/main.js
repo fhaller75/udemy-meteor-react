2896590942e8029fc71fea5def07dd99e6af287f
Frederic Haller <fhaller75@gmail.com>
2017-01-27 00:04:52 +0100

    92. Default Components with IndexRoutes

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
+import BinsMain from './components/bins/bins_main';
+import BinsList from './components/bins/bins_list';
 import { Bins } from '../imports/collections/bins';
 
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={BinsList}></IndexRoute>
+      <Route path="bins" component={BinsMain}></Route>
+    </Route>  
+  </Router>
+);
+
 Meteor.startup(() => {
-  ReactDom.render(<App/>, document.querySelector('.render-target'));
+  ReactDom.render(routes, document.querySelector('.render-target'));
 });
